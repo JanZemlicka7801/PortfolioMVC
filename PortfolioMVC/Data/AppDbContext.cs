d338767275719df6620d150988e44d2fe0d2ed04
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-16 13:33:36 +0000

    I have added database context for creating the entities inside the database.

---
+++ PortfolioMVC/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioMVC.Models;
+
+namespace PortfolioMVC.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Manager> Managers { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TeamMember> TeamMembers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Project>().HasOne(p => p.Manager).WithMany().HasForeignKey(p => p.ManagerId).OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TeamMember>().HasOne(t => t.Project).WithMany().HasForeignKey(t => t.ProjectId).OnDelete(DeleteBehavior.Cascade);
+    }
+}
