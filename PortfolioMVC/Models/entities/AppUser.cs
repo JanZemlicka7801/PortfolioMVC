e594c25b76988fcd32a17720ade0c36beb7e20e6
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-17 01:20:38 +0000

    Created a new class for user, deleted old manager entity

---
+++ PortfolioMVC/Models/entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PortfolioMVC.Models.Enums;
+
+namespace PortfolioMVC.Models.entities;
+
+public class AppUser : IdentityUser
+{
+    public string Name { get; set; }
+    public Department Department { get; set; }
+    public string Picture { get; set; }
+}
