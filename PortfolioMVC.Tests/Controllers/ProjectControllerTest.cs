8316bab6251a64bb3ca56b098124ca2422024d48
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-17 00:34:55 +0000

    Created unit tests for TeamMemberController and ProjectController, covering CRUD operations.

---
+++ PortfolioMVC.Tests/Controllers/ProjectControllerTest.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
+using PortfolioMVC.Controllers;
+using PortfolioMVC.Data;
+using PortfolioMVC.Models.DTOs;
+using PortfolioMVC.Models.entities;
+using Xunit;
+
+namespace PortfolioMVC.Tests.Controllers;
+
+[TestSubject(typeof(ProjectController))]
+public class ProjectControllerTest
+{
+
+    [Fact]
+    public async Task GetProjects_Success()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>
+        {
+            new Project 
+            { 
+                Id = 1, 
+                Name = "Project 1", 
+                Description = "Description 1", 
+                StartDate = DateTime.Today, 
+                EndDate = DateTime.Today.AddDays(30), 
+                ManagerId = 10 
+            },
+            new Project 
+            { 
+                Id = 2, 
+                Name = "Project 2", 
+                Description = "Description 2", 
+                StartDate = DateTime.Today.AddDays(1), 
+                EndDate = DateTime.Today.AddDays(31), 
+                ManagerId = 20 
+            }
+        };
+
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+
+        var controller = new ProjectController(mockContext.Object);
+
+        var result = await controller.GetProjects();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var returnValue = Assert.IsAssignableFrom<IEnumerable<ProjectDto>>(okResult.Value);
+        Assert.Equal(projects.Count, returnValue.Count());
+        Assert.Equal(projects.First().Name, returnValue.First().Name);
+    }
+
+    [Fact]
+    public async Task GetProject_Success()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>
+        {
+            new Project 
+            { 
+                Id = 1, 
+                Name = "Project 1", 
+                Description = "Description 1", 
+                StartDate = DateTime.Today, 
+                EndDate = DateTime.Today.AddDays(30), 
+                ManagerId = 10 
+            },
+            new Project 
+            { 
+                Id = 2, 
+                Name = "Project 2", 
+                Description = "Description 2", 
+                StartDate = DateTime.Today.AddDays(1), 
+                EndDate = DateTime.Today.AddDays(31), 
+                ManagerId = 20 
+            }
+        };
+
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+
+        var controller = new ProjectController(mockContext.Object);
+        int testId = 1;
+
+        var result = await controller.GetProject(testId);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var projectDto = Assert.IsType<ProjectDto>(okResult.Value);
+        Assert.Equal(testId, projectDto.Id);
+        Assert.Equal(projects.First(p => p.Id == testId).Name, projectDto.Name);
+    }
+
+    [Fact]
+    public async Task GetProject_Fail()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+
+        var controller = new ProjectController(mockContext.Object);
+        int testId = 999;
+        var result = await controller.GetProject(testId);
+
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task CreateProject_Fail()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var mockContext = new Mock<AppDbContext>(options);
+        
+        var projects = new List<Project>();
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        
+        var controller = new ProjectController(mockContext.Object);
+        controller.ModelState.AddModelError("Name", "Required");
+        
+        var newProjectDto = new ProjectDto
+        {
+            Name = "",
+            Description = "Some description",
+            StartDate = DateTime.Today,
+            EndDate = DateTime.Today.AddDays(30),
+            ManagerId = 10
+        };
+
+        var result = await controller.CreateProject(newProjectDto);
+        Assert.IsType<BadRequestObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task CreateProject_Success()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>();
+        var mockContext = new Mock<AppDbContext>(options);
+        
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockDbSet.Setup(m => m.Add(It.IsAny<Project>()))
+                 .Callback<Project>(p => { p.Id = 42; projects.Add(p); });
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+        
+        var controller = new ProjectController(mockContext.Object);
+        var newProjectDto = new ProjectDto
+        {
+            Name = "New Project",
+            Description = "New project description",
+            StartDate = DateTime.Today,
+            EndDate = DateTime.Today.AddDays(45),
+            ManagerId = 10
+        };
+
+        var result = await controller.CreateProject(newProjectDto);
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var returnDto = Assert.IsType<ProjectDto>(createdAtActionResult.Value);
+        Assert.Equal(42, returnDto.Id);
+        Assert.Equal(newProjectDto.Name, returnDto.Name);
+    }
+    
+    [Fact]
+    public async Task PutProject_Fail()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        
+        var projects = new List<Project>
+        {
+            new Project 
+            { 
+                Id = 1, 
+                Name = "Original Project", 
+                Description = "Original Description", 
+                StartDate = DateTime.Today, 
+                EndDate = DateTime.Today.AddDays(30), 
+                ManagerId = 10 
+            }
+        };
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+
+        var controller = new ProjectController(mockContext.Object);
+        var updateDto = new ProjectDto
+        {
+            Id = 2,
+            Name = "Updated Project",
+            Description = "Updated Description",
+            StartDate = DateTime.Today.AddDays(1),
+            EndDate = DateTime.Today.AddDays(31),
+            ManagerId = 20
+        };
+
+        var result = await controller.PutProject(1, updateDto);
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public async Task PutProject_NotExist()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.Projects.FindAsync(1)).ReturnsAsync((Project)null);
+        
+        var controller = new ProjectController(mockContext.Object);
+        var updateDto = new ProjectDto
+        {
+            Id = 1,
+            Name = "Updated Project",
+            Description = "Updated Description",
+            StartDate = DateTime.Today.AddDays(1),
+            EndDate = DateTime.Today.AddDays(31),
+            ManagerId = 20
+        };
+        var result = await controller.PutProject(1, updateDto);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task PutProject_Success()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        
+        var projects = new List<Project>
+        {
+            new Project 
+            { 
+                Id = 1, 
+                Name = "Original Project", 
+                Description = "Original Description", 
+                StartDate = DateTime.Today, 
+                EndDate = DateTime.Today.AddDays(30), 
+                ManagerId = 10 
+            }
+        };
+        
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.Projects.FindAsync(1)).ReturnsAsync(projects.First());
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var controller = new ProjectController(mockContext.Object);
+        var updateDto = new ProjectDto
+        {
+            Id = 1,
+            Name = "Updated Project",
+            Description = "Updated Description",
+            StartDate = DateTime.Today.AddDays(1),
+            EndDate = DateTime.Today.AddDays(31),
+            ManagerId = 20
+        };
+        var result = await controller.PutProject(1, updateDto);
+        var updatedProject = projects.First();
+
+        Assert.IsType<NoContentResult>(result);
+        Assert.Equal(updateDto.Name, updatedProject.Name);
+        Assert.Equal(updateDto.Description, updatedProject.Description);
+        Assert.Equal(updateDto.StartDate, updatedProject.StartDate);
+        Assert.Equal(updateDto.EndDate, updatedProject.EndDate);
+        Assert.Equal(updateDto.ManagerId, updatedProject.ManagerId);
+    }
+
+    [Fact]
+    public async Task DeleteProject_Fail()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.Projects.FindAsync(1)).ReturnsAsync((Project)null);
+        
+        var controller = new ProjectController(mockContext.Object);
+        var result = await controller.DeleteProject(1);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task DeleteProject_Success()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var projects = new List<Project>
+        {
+            new Project 
+            { 
+                Id = 1, 
+                Name = "Project to Delete", 
+                Description = "Description", 
+                StartDate = DateTime.Today, 
+                EndDate = DateTime.Today.AddDays(30), 
+                ManagerId = 10 
+            }
+        };
+        
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = projects.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.Projects).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.Projects.FindAsync(1)).ReturnsAsync(projects.First());
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var controller = new ProjectController(mockContext.Object);
+        var result = await controller.DeleteProject(1);
+        
+        Assert.IsType<NoContentResult>(result);
+        
+        mockDbSet.Verify(m => m.Remove(It.IsAny<Project>()), Times.Once());
+    }
+}
---
+++ PortfolioMVC.Tests/Controllers/TeamMemberControllerTest.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
+using PortfolioMVC.Controllers;
+using PortfolioMVC.Data;
+using PortfolioMVC.Models.DTOs;
+using PortfolioMVC.Models.entities;
+using Xunit;
+
+namespace PortfolioMVC.Tests.Controllers;
+
+[TestSubject(typeof(TeamMemberController))]
+public class TeamMemberControllerTest
+{
+
+    [Fact]
+    public async Task DeleteTeamMember_IsDeleted()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+            
+        var teamMembers = new List<TeamMember>
+        {
+            new TeamMember { Id = 1, Name = "Team Member 1", Role = Role.Developer, Email = "tm1@test.com", ProjectId = 10 }
+        };
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.TeamMembers.FindAsync(1)).ReturnsAsync(teamMembers.First());
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var controller = new TeamMemberController(mockContext.Object);
+        var result = await controller.DeleteTeamMember(1);
+
+        Assert.IsType<NoContentResult>(result);
+        mockDbSet.Verify(m => m.Remove(It.IsAny<TeamMember>()), Times.Once());
+    }
+    
+    [Fact]
+    public async Task DeleteTeamMember_NotExist()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var teamMembers = new List<TeamMember>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.TeamMembers.FindAsync(1)).ReturnsAsync((TeamMember)null);
+
+        var controller = new TeamMemberController(mockContext.Object);
+        var result = await controller.DeleteTeamMember(1);
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task UpdateTeamMember_NotMatch()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var teamMembers = new List<TeamMember>
+        {
+            new TeamMember { Id = 1, Name = "Team Member 1", Role = Role.Developer, Email = "tm1@test.com", ProjectId = 10 }
+        };
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+
+        var controller = new TeamMemberController(mockContext.Object);
+        var updateDto = new TeamMemberDto
+        {
+            Id = 2,
+            Name = "Updated Name",
+            Role = Role.Tester,
+            Email = "updated@test.com",
+            ProjectId = 20
+        };
+
+        var result = await controller.UpdateTeamMember(1, updateDto);
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public async Task UpdateTeamMember_NotExist()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var teamMembers = new List<TeamMember>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.TeamMembers.FindAsync(1)).ReturnsAsync((TeamMember)null);
+
+        var controller = new TeamMemberController(mockContext.Object);
+        var updateDto = new TeamMemberDto
+        {
+            Id = 1,
+            Name = "Updated Name",
+            Role = Role.Tester,
+            Email = "updated@test.com",
+            ProjectId = 20
+        };
+
+        var result = await controller.UpdateTeamMember(1, updateDto);
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task UpdateTeamMember_Successful()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var teamMembers = new List<TeamMember>
+        {
+            new TeamMember { Id = 1, Name = "Team Member 1", Role = Role.Developer, Email = "tm1@test.com", ProjectId = 10 }
+        };
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.TeamMembers.FindAsync(1)).ReturnsAsync(teamMembers.First());
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var controller = new TeamMemberController(mockContext.Object);
+        var updateDto = new TeamMemberDto
+        {
+            Id = 1,
+            Name = "Updated Team Member",
+            Role = Role.Tester,
+            Email = "updated@test.com",
+            ProjectId = 20
+        };
+
+        var result = await controller.UpdateTeamMember(1, updateDto);
+        var updatedMember = teamMembers.First();
+        
+        Assert.IsType<NoContentResult>(result);
+        Assert.Equal(updateDto.Name, updatedMember.Name);
+        Assert.Equal(updateDto.Role, updatedMember.Role);
+        Assert.Equal(updateDto.Email, updatedMember.Email);
+        Assert.Equal(updateDto.ProjectId, updatedMember.ProjectId);
+    }
+
+    [Fact]
+    public async Task CreateTeamMember_Invalid()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var mockContext = new Mock<AppDbContext>(options);
+        
+        var teamMembers = new List<TeamMember>();
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        var controller = new TeamMemberController(mockContext.Object);
+        
+        controller.ModelState.AddModelError("Name", "Required");
+        var newTeamMemberDto = new TeamMemberDto
+        {
+            Name = "",
+            Role = Role.Developer,
+            Email = "tm@test.com",
+            ProjectId = 10
+        };
+        var result = await controller.CreateTeamMember(newTeamMemberDto);
+
+        Assert.IsType<BadRequestObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task CreateTeamMember_ValidTeamMember()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var teamMembers = new List<TeamMember>();
+        var mockContext = new Mock<AppDbContext>(options);
+        var mockDbSet = teamMembers.AsQueryable().BuildMockDbSet();
+        
+        mockDbSet.Setup(m => m.Add(It.IsAny<TeamMember>()))
+                 .Callback<TeamMember>(tm => { tm.Id = 99; teamMembers.Add(tm); });
+        mockContext.Setup(c => c.TeamMembers).Returns(mockDbSet.Object);
+        mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+        
+        var controller = new TeamMemberController(mockContext.Object);
+        var newTeamMemberDto = new TeamMemberDto
+        {
+            Name = "New Team Member",
+            Role = Role.Developer,
+            Email = "newtm@test.com",
+            ProjectId = 10
+        };
+        var result = await controller.CreateTeamMember(newTeamMemberDto);
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var returnDto = Assert.IsType<TeamMemberDto>(createdAtActionResult.Value);
+        
+        Assert.Equal(99, returnDto.Id);
+        Assert.Equal(newTeamMemberDto.Email, returnDto.Email); 
+    }
+}
