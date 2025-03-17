ec80c5bea65db5d7d1aff8cd058cb0dedcc75261
Malikom12 <muhammadmalikomer@gmail.com>
2025-03-17 16:25:59 +0000

    Created models for profile change password and settings

---
+++ PortfolioMVC/Models/Enums/ProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PortfolioMVC.Models.Enums;
+
+namespace PortfolioMVC.Controllers.Views;
+
+public class ProfileViewModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public Department Department { get; set; }
+}
---
+++ PortfolioMVC/Models/Enums/SettingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PortfolioMVC.Models.Enums;
+
+namespace PortfolioMVC.Controllers.Views;
+
+public class SettingsViewModel
+{
+    [Required]
+    [Display(Name = "Full Name")]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public Department Department { get; set; }
+}
