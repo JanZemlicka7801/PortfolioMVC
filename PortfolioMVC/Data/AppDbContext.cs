b039aa77824355a62d5ad21437af92d6693b7001
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-17 03:18:52 +0000

    Refactor AppDbContext to use IdentityDbContext.

--- PortfolioMVC/Data/AppDbContext.cs
+++ PortfolioMVC/Data/AppDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PortfolioMVC.Models.entities;
 
 namespace PortfolioMVC.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     
-    public virtual DbSet<Manager> Managers { get; set; }
     public virtual DbSet<Project> Projects { get; set; }
     public virtual DbSet<TeamMember> TeamMembers { get; set; }
 
@@ -15,8 +15,7 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Project>().HasOne(p => p.Manager).WithMany().HasForeignKey(p => p.ManagerId).OnDelete(DeleteBehavior.Restrict);
-
+        modelBuilder.Entity<Project>().HasOne(p => p.Manager).WithMany(m => m.Projects).HasForeignKey(p => p.ManagerId).OnDelete(DeleteBehavior.Restrict);
         modelBuilder.Entity<TeamMember>().HasOne(t => t.Project).WithMany().HasForeignKey(t => t.ProjectId).OnDelete(DeleteBehavior.Cascade);
     }
 }
