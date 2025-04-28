b10709c044d0f727619db9311437b71f6be8beaf
Malikom12 <muhammadmalikomer@gmail.com>
2025-04-28 03:48:50 +0100

    View Models

---
+++ PortfolioMVC/Models/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PortfolioMVC.Models.Enums;
+
+namespace PortfolioMVC.Models.ViewModels
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Department is required.")]
+        [Display(Name = "Department")]
+        public Department Department { get; set; }
+
+        [Display(Name = "Roles")]
+        public List<string> UserRoles { get; set; } = new List<string>();
+
+        public List<string> AllRoles { get; set; } = new List<string>();
+    }
+}
---
+++ PortfolioMVC/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PortfolioMVC.Models.Enums;
+
+namespace PortfolioMVC.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public Department Department { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
