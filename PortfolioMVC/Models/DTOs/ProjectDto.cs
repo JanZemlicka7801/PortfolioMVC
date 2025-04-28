76a65c61d1c140bbf0910dcfa0d5c21629b10bbd
Malikom12 <muhammadmalikomer@gmail.com>
2025-04-28 03:47:08 +0100

    DTOS

--- PortfolioMVC/Models/DTOs/ProjectDto.cs
+++ PortfolioMVC/Models/DTOs/ProjectDto.cs
@@ -5,19 +5,19 @@ namespace PortfolioMVC.Models.DTOs;
 public class ProjectDto
 {
     public int Id { get; set; }
-    
+
     [Required]
     [StringLength(100)]
     public string? Name { get; set; }
-    
+
     [StringLength(500)]
     public string? Description { get; set; }
-    
+
     [DataType(DataType.Date)]
     public DateTime StartDate { get; set; }
-    
+
     [DataType(DataType.Date)]
     public DateTime EndDate { get; set; }
-    
+
     public string? ManagerId { get; set; }
 }
--- PortfolioMVC/Models/DTOs/TeamMemberDto.cs
+++ PortfolioMVC/Models/DTOs/TeamMemberDto.cs
@@ -6,17 +6,17 @@ namespace PortfolioMVC.Models.DTOs;
 public class TeamMemberDto
 {
     public int Id { get; set; }
-    
+
     [Required]
     [StringLength(100)]
     public string? Name { get; set; }
-    
+
     [Required]
     public Role Role { get; set; }
-    
+
     [Required]
     [EmailAddress]
     public string? Email { get; set; }
-    
+
     public int ProjectId { get; set; }
 }
